66ccfdae64facd9208ae8e8d8fe67540a0d620b5
Evan Chute <echute@ginkgostreet.com>
2018-11-20 19:17:13 -0500

    use entity_table and entity_id columns to getParticipantCount

--- CRM/Rbp/Util.php
+++ CRM/Rbp/Util.php
@@ -10,7 +10,8 @@ class CRM_Rbp_Util {
    */
   public static function getParticipantCount(CRM_CiviDiscount_DAO_Track $discountTrack) {
     $lineItems = civicrm_api3('LineItem', 'get', array(
-      'contribution_id' => $discountTrack->contribution_id,
+      'entity_table' => $discountTrack->entity_table,
+      'entity_id' => $discountTrack->entity_id,
     ));
 
     return (int) array_sum(array_column($lineItems['values'], 'participant_count'));
